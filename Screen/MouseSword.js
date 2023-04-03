caef9a75744ac02d1ba097d7cb9ddcc64e3345f0
Bubba <114591669+DevBubba@users.noreply.github.com>
2023-04-03 13:16:06 -0500

    Create MouseSword.js

---
+++ Screen/MouseSword.js
@@ -0,0 +1,1 @@
+javascript:iV33MaET=0;Cu4Xg8Y=new Array('n-resize','nw-resize','w-resize','sw-resize','s-resize','se-resize','e-resize','ne-resize');setInterval('iV33MaET++;document.body.style.cursor=Cu4Xg8Y[iV33MaET%8]',150)
