26db424c8d77680e2e2a2c6d968d0348b833b2a3
Bubba <114591669+DevBubba@users.noreply.github.com>
2023-04-03 13:17:00 -0500

    Create UwU.js

---
+++ Screen/UwU.js
@@ -0,0 +1,1 @@
+javascript: const faces = ['UwU']; let face = faces[Math.floor(Math.random()*faces.length)]; document.write('<body style=\'background-color: black;\'></body><title>' + face + '</title><p style=\'text-align: center; font-size: 500px; font-family: Arial; color: white;\'>' + face + '</p>');
