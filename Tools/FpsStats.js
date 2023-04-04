969024d584f047baaa35107152a998d719aa8e23
Bubba <114591669+DevBubba@users.noreply.github.com>
2023-04-04 11:32:22 -0500

    Create FpsStats.js

---
+++ Tools/FpsStats.js
@@ -0,0 +1,1 @@
+javascript:(function(){var script=document.createElement('script');script.onload=function(){var stats=new Stats();document.body.appendChild(stats.dom);requestAnimationFrame(function loop(){stats.update();requestAnimationFrame(loop)});};script.src='https://mrdoob.github.io/stats.js/build/stats.min.js';document.head.appendChild(script);})()
