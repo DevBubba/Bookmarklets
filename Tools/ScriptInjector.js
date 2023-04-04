36fa0cb46d783274aeca07a6234d16383d6d4b66
Bubba <114591669+DevBubba@users.noreply.github.com>
2023-04-04 11:31:14 -0500

    Create ScriptInjector.js

---
+++ Tools/ScriptInjector.js
@@ -0,0 +1,1 @@
+javascript:function JSexe(){ if(valJS!='null' && valJS!='undefined')strJS=valJS; strJS=prompt('Enter Javascript Or Script To Inject:',strJS); if(strJS!=null && strJS!='' && strJS!='undefined'){  setTimeout('valJS=\'\'+eval(strJS);JSexe()',10); } else{valJS='';strJS='';}}valJS='';strJS='';JSexe();
