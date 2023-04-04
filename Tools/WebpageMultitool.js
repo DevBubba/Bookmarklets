2d6d849c16992760d7f7fc0c6cc61b33525a3aeb
Bubba <114591669+DevBubba@users.noreply.github.com>
2023-04-04 10:52:02 -0500

    Create WebpageMultitool.js

---
+++ Tools/WebpageMultitool.js
@@ -0,0 +1,1 @@
+javascript:(function () %7Bvar v %3D document.createElement(%27script%27)%3Bv.src %3D %27https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FBrowncha023%2FVengeance%40v1.2.0%2Fscript.min.js%27%3Bdocument.body.appendChild(v)%3B%7D())
